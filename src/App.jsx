0136625827926f440094ccceadc221b78473b5ad
vincenzo-terracciano <vincenzo.terracciano97@gmail.com>
2025-03-28 17:04:55 +0100

    Refactor API endpoints and enhance post display with a styled table

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from 'react'
-const api_endpoint = 'http://localhost:3000/api/v1/posts'
+const api_endpoint = 'http://localhost:3000'
+const post_endpoint = '/api/v1/posts'
 
 function App() {
+  const [posts, setPosts] = useState([])
 
   useEffect(() => {
-    fetchData(api_endpoint)
+    fetchData(api_endpoint + post_endpoint)
   }, [])
 
   function fetchData(url) {
@@ -12,14 +14,45 @@ function App() {
       .then(res => res.json())
       .then(data => {
         console.log(data);
-
+        setPosts(data)
 
       })
   }
 
   return (
     <>
-      <h1>Blog API</h1>
+      <h1 className='text-center my-3'>Blog API</h1>
+      <section className='posts'>
+        <div className="container">
+          <table className='table table-striped'>
+            <thead>
+              <tr>
+                <th scope='col'>#</th>
+                <th scope='col'>Product</th>
+                <th scope='col'>Image</th>
+                <th scope='col'>Content</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                posts.map((post, index) => (
+                  <tr key={`post - ${index + 1}`}>
+                    <th scope='row'>{index + 1}</th>
+                    <td>{post.title}</td>
+                    <td>
+                      {console.log(api_endpoint + post.image)
+                      }
+                      <img src={api_endpoint + post.image} alt={post.title} />
+                    </td>
+                    <td>{post.content}</td>
+                  </tr>
+                ))
+              }
+            </tbody>
+          </table>
+        </div>
+      </section>
+
     </>
   )
 }
